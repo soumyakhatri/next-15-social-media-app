7cdda10f2ebc7a55aac814dc5d78b083636549a9
Soumya Khatri <soumya91rias@gmail.com>
2025-12-31 13:03:11 +0530

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | bugbook",
-    default: "bugbook",
+    template: "%s | earbook",
+    default: "earbook",
   },
   description: "The social media app for powernerds",
 };
