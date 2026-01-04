5ee3068a5c9cffce821e4f3071795ed1691a1ce1
Soumya Khatri <soumya91rias@gmail.com>
2026-01-04 10:01:47 +0530

    trending topics sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/types";
 
@@ -12,7 +13,7 @@ export default async function Home() {
   });
 
   return (
-    <main className="w-full min-w-0">
+    <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         <div className="space-y-5">
@@ -21,6 +22,7 @@ export default async function Home() {
           ))}
         </div>
       </div>
+      <TrendsSidebar/>
     </main>
   );
 }
