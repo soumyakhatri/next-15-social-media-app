7b1c217ab35edbe3cce62bdd104a20e1b571e24b
Soumya Khatri <soumya91rias@gmail.com>
2026-01-08 15:33:15 +0530

    Following Feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
   return (
@@ -8,7 +10,22 @@ export default function Home() {
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         <div className="space-y-5">
-          <ForYouFeed/>
+          <Tabs defaultValue="for-you">
+            <TabsList>
+              <TabsTrigger value="for-you">
+                For You
+              </TabsTrigger>
+              <TabsTrigger value="following">
+                Following
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="for-you">
+              <ForYouFeed/>
+            </TabsContent>
+            <TabsContent value="following">
+              <FollowingFeed/>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
       <TrendsSidebar/>
